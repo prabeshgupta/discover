14a2e6edfb09486da7bd0f6df653f4543efea5a3
Prabesh Gupta <prabeshtech232@gmail.com>
2024-01-25 19:00:56 +0545

    Discover nav responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
                 <Image src='/logo.png' alt="Logo" width={50} height={50} />
                 <h2 className='text-[25px] bg-gradient-to-l from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text tracking-widest font-semibold'>DISCOVER</h2>
             </div>
-            <ul className='flex gap-8 items-center'>
+            <ul className='flex gap-8 items-center max-sm:hidden'>
                 <li className='text-[18px] cursor-pointer text-indigo-500'><a href="https://np.linkedin.com/in/prabeshgupta">Linkedin</a></li>
                 <li className='text-[18px] cursor-pointer text-purple-500'><a href="https://github.com/prabeshgupta/discover">Github</a></li>
                 <li className='text-[18px] cursor-pointer text-pink-500'><a href="mailto:admin@prabeshgupta.com.np">Contact</a></li>
