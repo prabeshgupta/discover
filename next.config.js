c0cc9660986e0ea2c61e708d7babaeea70605a8d
Prabesh Gupta <prabeshtech232@gmail.com>
2024-01-26 10:56:24 +0545

    Discover

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['maps.googleapis.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'maps.googleapis.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     }
 };
 
